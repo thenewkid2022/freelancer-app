3908afb05cd2ecb19174ed572497c1c32c796aaa
Christian von Ow <christianvonow@Christians-MacBook-Pro.local>
2025-05-01 00:27:25 +0700

    chore: Remove old project files and configurations

--- src/components/Login.js
+++
@@ -1,8 +0,0 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext';
-import { Link } from 'react-router-dom';
-
-function Login() {
-  // ... existing code ...
-} 
