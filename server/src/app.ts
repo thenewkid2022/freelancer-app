1a9f63be40786664eeec839331d4c25e2451dd0f
Christian von Ow <christianvonow@Christians-MacBook-Pro.local>
2025-05-02 17:17:27 +0700

    fix: X-Requested-With zu erlaubten CORS-Headers hinzugefügt

--- server/src/app.ts
+++ server/src/app.ts
@@ -32,7 +32,7 @@ app.use(cors({
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'Accept', 'X-Requested-With'],
   maxAge: 86400
 }));
 
